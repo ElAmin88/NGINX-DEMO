a6ff7208c8e841d747daf79fd3706145cc7dd536
Abdel-Rhman El-Amin <abdel-rhman.el-amin@ibm.com>
2021-05-10 09:08:50 +0200

    fixed bat file

--- if.bat
+++ if.bat
@@ -7,5 +7,5 @@ If "%containerId%" == "" (
   docker stop %ContainerId%
   docker rm -f %ContainerId%
 )
-docker build -t myapp .
+docker build -t myapp https://github.com/ElAmin88/NGINX-DEMO
 docker run --name myapp -p 3000:80 -d myapp 
